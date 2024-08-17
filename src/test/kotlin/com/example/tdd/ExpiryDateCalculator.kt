9200fdb00aafd4dda0e03067be2db7dbb4874775
ara-zo <jo91kr@gmail.com>
2024-08-17 16:11:09 +0900

    :recycle: 리팩토링 (상수를 변수로)

--- src/test/kotlin/com/example/tdd/ExpiryDateCalculator.kt
+++ src/test/kotlin/com/example/tdd/ExpiryDateCalculator.kt
@@ -2,17 +2,18 @@ package com.example.tdd
 
 import org.springframework.stereotype.Service
 import java.time.LocalDate
-import kotlin.test.Test
 
 @Service
 class ExpiryDateCalculator {
 
     fun calculateExpiryDate(payData: PayData): LocalDate {
+        val addedMonths: Long = 1
+
         payData.firstBillingDate?.let {
             // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
 
             // 후보 만료일 구함
-            val candidateExp = payData.billingDate.plusMonths(1)
+            val candidateExp = payData.billingDate.plusMonths(addedMonths)
 
             // 첫 납부일의 일자와 후보 만료일의 일자가 다르면,
             if(payData.firstBillingDate.dayOfMonth != candidateExp.dayOfMonth) {
@@ -23,7 +24,7 @@ class ExpiryDateCalculator {
             }
         }
 
-        return payData.billingDate.plusMonths(1)
+        return payData.billingDate.plusMonths(addedMonths)
     }
 
 }
