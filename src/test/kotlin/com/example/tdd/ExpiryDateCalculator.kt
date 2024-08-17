2bb0e0b6d3bc8c0bae2bf2a3aebb9ee8ea3a3652
ara-zo <jo91kr@gmail.com>
2024-08-17 17:01:55 +0900

    :recycle: 리펙토링

--- src/test/kotlin/com/example/tdd/ExpiryDateCalculator.kt
+++ src/test/kotlin/com/example/tdd/ExpiryDateCalculator.kt
@@ -7,30 +7,43 @@ import java.time.YearMonth
 @Service
 class ExpiryDateCalculator {
 
-    fun calculateExpiryDate(payData: PayData): LocalDate {
+    fun calculateExpiryDate(payData: PayData): LocalDate? {
         val addedMonths: Int = payData.payAmount / 10_000
 
+        // 후보 만료일 구함
+        val candidateExp = payData.billingDate.plusMonths(addedMonths.toLong())
+
         payData.firstBillingDate?.let {
-            // 첫 납부일과 납부일의 일자가 다르면 첫 납부일의 일자를 만료일의 일자로 사용
-
-            // 후보 만료일 구함
-            val candidateExp = payData.billingDate.plusMonths(addedMonths.toLong())
-
-            // 첫 납부일의 일자와 후보 만료일의 일자가 다르면,
-            if(payData.firstBillingDate.dayOfMonth != candidateExp.dayOfMonth) {
-                // 후보 만료일이 포함된 달의 마지막날 < 첫 납부일의 일자
-                if(YearMonth.from(candidateExp).lengthOfMonth() < payData.firstBillingDate.dayOfMonth) {
-                    return candidateExp.withDayOfMonth(YearMonth.from(candidateExp).lengthOfMonth())
-                }
-
-                // 첫 납부일의 일자를 후보 만료일의 일자로 사용
-                return candidateExp.withDayOfMonth(
-                    payData.firstBillingDate.dayOfMonth
-                )
-            }
-        }
+            return expiryDateUsingFirstBillingDate(payData, candidateExp)
+        } ?: candidateExp
 
         return payData.billingDate.plusMonths(addedMonths.toLong())
     }
+
+    private fun expiryDateUsingFirstBillingDate(
+        payData: PayData,
+        candidateExp: LocalDate
+    ): LocalDate? {
+        val dayOfFirstBilling = payData.firstBillingDate!!.dayOfMonth
+
+        // 첫 납부일의 일자와 후보 만료일의 일자가 다르면,
+        if (isSameDayOfMonth(dayOfFirstBilling, candidateExp)) {
+            val dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth()
+
+            // 후보 만료일이 포함된 달의 마지막날 < 첫 납부일의 일자
+            if (lastDayOfMonth(dayLenOfCandiMon, dayOfFirstBilling)) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon)
+            }
+
+            // 첫 납부일의 일자를 후보 만료일의 일자로 사용
+            return candidateExp.withDayOfMonth(dayOfFirstBilling)
+        } else {
+            return candidateExp
+        }
+    }
+
+    private fun lastDayOfMonth(dayLenOfCandiMon: Int, dayOfFirstBilling: Int) = dayLenOfCandiMon < dayOfFirstBilling
+
+    private fun isSameDayOfMonth(dayOfFirstBilling: Int, candidateExp: LocalDate) = dayOfFirstBilling != candidateExp.dayOfMonth
 
 }
