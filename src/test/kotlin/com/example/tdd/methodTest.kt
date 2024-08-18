5aea3faf07bfbfe52aff72bd307ed90fddbd2d03
ara-zo <jo91kr@gmail.com>
2024-08-18 16:52:09 +0900

    :white_check_mark: 익셉션 발생 유무 검사 메서드

--- src/test/kotlin/com/example/tdd/methodTest.kt
+++ src/test/kotlin/com/example/tdd/methodTest.kt
@@ -11,18 +11,19 @@ import kotlin.IllegalArgumentException as IllegalArgumentException1
 class methodTest {
 
     @Test
-    fun `두 객체가 같은지 비교`() {
+    fun `두_객체가_같은지_비교`() {
         val dateTime1 = LocalDate.now()
         val dateTime2 = LocalDate.now()
         assertEquals(dateTime1, dateTime2)
     }
 
     @Test
-    fun `테스트 실패 알림`() {
+    fun `테스트_실패_알림`() {
         val thrown = assertThrows<IllegalArgumentException> {
             val dateTime1 = LocalDate.now()
             val dateTime2 = dateTime1.minusDays(1) // 날짜를 다르게 설정하여 테스트
 
+            // 이부분 안넣으면 AssertionFailedError가 뜸
             if (dateTime1 != dateTime2) {
                 throw IllegalArgumentException("dateTime1 and dateTime2 are not equal")
             }
@@ -31,7 +32,4 @@ class methodTest {
         }
         assertTrue(thrown.message?.contains("dateTime1") == true)
     }
-
-
-
 }
