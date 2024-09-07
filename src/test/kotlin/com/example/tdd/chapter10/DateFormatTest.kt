761ea2c7ecca18574db7ffd90d8de10861bfdcf1
ara-zo <jo91kr@gmail.com>
2024-09-08 01:16:08 +0900

    10.2 기대하는 값에 문자열 값을 사용한 예

--- src/test/kotlin/com/example/tdd/chapter10/DateFormatTest.kt
+++ src/test/kotlin/com/example/tdd/chapter10/DateFormatTest.kt
@@ -10,6 +10,7 @@ import kotlin.test.assertEquals
 class DateFormatTest {
 
     // 10.1 기대하는 값에 변수를 사용한 예
+    /*
     @Test
     fun dateFormat() {
         val date = LocalDate.of(1945, 8, 15)
@@ -19,6 +20,16 @@ class DateFormatTest {
             dateStr
         )
     }
+    */
+
+    // 10.2 기대하는 값에 문자열 값을 사용한 예
+    @Test
+    fun dateFormat() {
+        val date = LocalDate.of(1945, 8, 15)
+        val dateStr = formatDate(date)
+        assertEquals("1945년 8월 15일", dateStr)
+    }
+
 
     private fun formatDate(date: LocalDate): String {
         return "${date.year}년 ${date.monthValue}월 ${date.dayOfMonth}일"
