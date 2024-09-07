b15fb994e1a983f046a9577cdcc3e1f751631075
ara-zo <jo91kr@gmail.com>
2024-09-08 01:26:51 +0900

    10.3~4

--- src/test/kotlin/com/example/tdd/chapter10/변수나 필드를 사용해서 기댓값 표현하지 않기.kt
+++ src/test/kotlin/com/example/tdd/chapter10/변수나 필드를 사용해서 기댓값 표현하지 않기.kt
@@ -1,13 +1,13 @@
 package com.example.tdd.chapter10
 
+import org.junit.jupiter.api.assertAll
 import org.springframework.boot.test.context.SpringBootTest
 import java.time.LocalDate
-import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @SpringBootTest
-class DateFormatTest {
+class `변수나 필드를 사용해서 기댓값 표현하지 않기` {
 
     // 10.1 기대하는 값에 변수를 사용한 예
     /*
@@ -28,6 +28,28 @@ class DateFormatTest {
         val date = LocalDate.of(1945, 8, 15)
         val dateStr = formatDate(date)
         assertEquals("1945년 8월 15일", dateStr)
+    }
+
+    /*
+    // 10.3 단언과 객체 생성에 필드와 변수를 사용한 코드
+    @Test
+    fun checkArray() {
+        assertAll(
+            {assertEquals(answers[0], resultedAnswers[0])},
+            { assertEquals(answers[1], resultedAnswers[1]) },
+            { assertEquals(answers[2], resultedAnswers[2]) }
+        )
+    }
+    */
+
+    // 10.4 객체 생성과 단언에서 변수 대신 값을 사용
+    @Test
+    fun checkArray() {
+        assertAll(
+            { assertEquals(1, resultedAnswers[0]) },
+            { assertEquals(2, resultedAnswers[1]) },
+            { assertEquals(3, resultedAnswers[2]) }
+        )
     }
 
 
